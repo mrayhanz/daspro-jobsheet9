af6551d0e711ba0b64ff47b78815619536589c46
unknown <rayymekdi@gmail.com>
2024-10-31 09:43:33 +0700

    pertanyaan4

--- SearchNilai17.java
+++ SearchNilai17.java
@@ -1,17 +1,33 @@
+import java.util.Scanner;
+
 public class SearchNilai17 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
+        Scanner sc = new Scanner(System.in);
+        int key;
         int hasil = 0;
 
+        System.out.print("Masukkan banyaknya nilai yang akan di input : ");
+        int input = sc.nextInt();
+        int[] arrNilai = new int[input];
+
+        for(int i = 0; i < arrNilai.length; i ++){
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            arrNilai[i] = sc.nextInt();
+        }
+        System.out.print("Masukkan nilai yang ingin di cari : ");
+        key = sc.nextInt();
         for(int i = 0; i < arrNilai.length; i ++){
             if (key == arrNilai[i]) {
                 hasil = i;
                 break;
             }
         }
-        System.out.println();
-        System.out.println("Nilai " + key + " Ketemu di indeks ke-" + hasil);
-        System.out.println();
+        if (hasil == 0) {
+            System.out.println();
+            System.out.println("Nilai yang dicari tidak di termukan");
+        } else {
+            System.out.println();
+            System.out.println("Nilai " + key + "ketemu , Merupakan nilai mahasiswa ke-" + (hasil+1));
+        }
     }
 }
